dab9e839ae58e7d3e69f4340c61e5098ed687431
Edenzzzz <wenxuan.tan@wisc.edu>
2022-12-10 21:48:50 +0000

    fixes

--- sd-inference.py
+++ sd-inference.py
@@ -315,7 +315,7 @@ def visualize_prompts(
             print(text[index:index+args.batch_size])
             images+=pipeline(text[index:index+args.batch_size],height=img_size,width=img_size,
                             num_inference_steps=50, guidance_scale=7.5,
-                            latents=latents[index:index+args.batch_size], padding="longest").images
+                            latents=latents[index:index+args.batch_size]).images
             index = index+args.batch_size
         else:#To avoid out of memory, generate one at a time
           for j in range(samples_per_prompt):
